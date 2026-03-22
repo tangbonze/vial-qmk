5f3aa532f495146177c407789f6a9c90de3efcc1
tangbonze <fengk@outlook.com>
2026-03-22 17:10:25 +0800

    feat: Enable QMK settings, tap dance, combos, and key override features.

--- keyboards/keyball/keyball61/keymaps/vial/rules.mk
+++ keyboards/keyball/keyball61/keymaps/vial/rules.mk
@@ -7,7 +7,7 @@ VIAL_ENABLE = yes
 
 # Vial optimizations to save space
 LTO_ENABLE = yes
-QMK_SETTINGS = no
-TAP_DANCE_ENABLE = no
-COMBO_ENABLE = no
-KEY_OVERRIDE_ENABLE = no
+QMK_SETTINGS = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
